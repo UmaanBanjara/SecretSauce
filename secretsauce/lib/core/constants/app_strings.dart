6a4f0796444a28d1482f533d68f83f3a3e1d5b5c
Umaan Banjara <umaanbanjara.com>
2025-10-23 09:03:42 +0545

    app string constants

---
+++ secretsauce/lib/core/constants/app_strings.dart
@@ -0,0 +1,25 @@
+class AppStrings{
+  static final String CreateAccount = 'Create an account';
+  static final String Email = 'Email';
+  static final String EnterEmail = 'Enter your email';
+  static final String FirstName = 'First Name';
+  static final String HintFirstName = 'Enter your first name';
+  static final String LastName = 'Last Name';
+  static final String HintLastName = 'Enter your last name';
+  static final String UserName = 'Username';
+  static final String HintUserName = 'Enter your username';
+  static final String Password = 'Password';
+  static final String HintPassword = 'Enter your password';
+  static final String HaveAccount = 'Already have an account?  ';
+  static final String Login = 'Login';
+  static final String Signup = 'Signup';
+  static final String TOS = 'Terms and Services';
+  static final String ByCreating = 'By creating a Secret Sauce account, you agree to our ';
+  static final String ByLogging = 'By logging in, you agree to our ';
+  static final String OtpVerification = 'Verify your OTP';
+  static final String EnterOtp = 'OTP';
+  static final String OtpHint = 'Enter your OTP';
+  static final String VerifyOtp = 'Verify';
+  static final String DidNotReceiveOtp = "Didn't receive OTP? ";
+  static final String Resend = 'Resend';
+}
