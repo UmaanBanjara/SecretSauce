253d613b8edff5af34418500b2e2394584dcd37e
Umaan Banjara <umaanbanjara.com>
2025-10-23 09:05:54 +0545

    app routes

---
+++ secretsauce/lib/core/routes/app_routes.dart
@@ -0,0 +1,28 @@
+import 'package:go_router/go_router.dart';
+import 'package:secretsauce/core/routes/routes_string.dart';
+import 'package:secretsauce/features/auth/presentation/screens/login_screen.dart';
+import 'package:secretsauce/features/auth/presentation/screens/otp_screen.dart';
+import 'package:secretsauce/features/auth/presentation/screens/signup_screen.dart';
+import 'package:secretsauce/features/auth/presentation/screens/tos.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: RoutesString.signup,
+  routes: [
+    GoRoute(
+      path : RoutesString.login,
+      builder: (context , state) => LoginScreen(),
+    ),
+    GoRoute(
+      path : RoutesString.tos,
+      builder : (context , state) => TosPage(),
+    ),
+    GoRoute(
+      path : RoutesString.otp,
+      builder : (context , state) => OtpScreen()
+    ),
+    GoRoute(
+      path : RoutesString.signup,
+      builder : (context , state) => SignupScreen()
+    )
+  ]
+);
