72f820431d5855cde74c6b9b3ea0104b3c974bee
Umaan Banjara <umaanbanjara.com>
2025-10-23 09:00:37 +0545

    added CORS integration

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.signup_route import router as signup_router
 from routes.login_route import router as login_router
 
 app = FastAPI(title="Secret Sauce")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(signup_router , prefix="/auth" , tags=["Authentication"])
 app.include_router(login_router , prefix="/auth" , tags=["Authorization"])
