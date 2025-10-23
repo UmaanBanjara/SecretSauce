bc809a7e4b0bee779af80e0666c8a824eb4b75c7
Umaan Banjara <umaanbanjara.com>
2025-10-23 09:05:27 +0545

    route strings

---
+++ secretsauce/lib/core/routes/routes_string.dart
@@ -0,0 +1,7 @@
+class RoutesString {
+  static final String signup = '/signup';
+  static final String login = '/login';
+  static final String tos = '/tos';
+  static final String otp = '/otp';
+  
+}
