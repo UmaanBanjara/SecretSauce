e6c264f9656b6d5534131d6a3e3cf2face6585ff
UmaanBanjara <154421467+UmaanBanjara@users.noreply.github.com>
2025-10-25 20:10:47 +0545

    Delete secretsauce/lib/routes directory

--- secretsauce/lib/routes/routes_string.dart
+++
@@ -1,7 +0,0 @@
-class RoutesString {
-  static final String signup = '/signup';
-  static final String login = '/login';
-  static final String tos = '/tos';
-  static final String otp = '/otp';
-  
-}
