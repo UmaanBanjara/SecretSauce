8700aeec75459e5eafb8b565b183b37952a7770d
Umaan Banjara <umaanbanjara.com>
2025-10-25 19:55:12 +0545

    main file that combines all different routes

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.signup_route import router as signup_router
 from routes.login_route import router as login_router
+from routes.verify_otp import router as otp_router
+from routes.resend_otp import router as resend_router
 
 app = FastAPI(title="Secret Sauce")
 
@@ -15,6 +17,8 @@ app.add_middleware(
 
 app.include_router(signup_router , prefix="/auth" , tags=["Authentication"])
 app.include_router(login_router , prefix="/auth" , tags=["Authorization"])
+app.include_router(otp_router , prefix="/auth" , tags=["Authorization"])
+app.include_router(resend_router , prefix="/auth" , tags=["Authorization"])
 
 @app.get("/")
 async def root():
