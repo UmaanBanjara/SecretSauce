ac37b34807d1cdb228e572ca938a3815b88b0837
Umaan Banjara <umaanbanjara.com>
2025-10-25 20:04:33 +0545

    model for retrieving api data

--- secretsauce/lib/features/data/models/message_response.dart
+++ secretsauce/lib/features/data/models/message_response.dart
@@ -1,16 +1,19 @@
 class MessageResponse {
   final String message;
   final int? userId;
+  final String? token;
 
   MessageResponse({
     required this.message,
-    this.userId
+    this.userId,
+    this.token,
   });
 
   factory MessageResponse.fromJson(Map<String , dynamic> json){
     return MessageResponse(
       message: json['message'],
-      userId: json['user_id']
+      userId: json['user_id'],
+      token: json['access_token']
     );
   }
 }
